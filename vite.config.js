6db634f2abaf9ea9d08f83ccc370e8cb6abbd67d
thanatpump1 <thanat.tcl@kkumail.com>
2025-03-26 22:27:11 +0700

    Add Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -7,7 +8,16 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
+        react(),
     ],
+    build: {
+        outDir: 'public/build',
+        assetsDir: '',
+        manifest: true,
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+        },
+    },
     esbuild: {
         loader: 'jsx',
         include: /.*\.jsx?$/,
