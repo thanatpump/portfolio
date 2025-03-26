3d42d443213b154baf21aa7fdd459875b3f16d6c
thanatpump1 <thanat.tcl@kkumail.com>
2025-03-26 22:56:21 +0700

    Update deployment configuration and dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <!-- resources/views/welcome.blade.php -->
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio</title>
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
-</head>
-<body>
-    <div id="app"></div>  <!-- React จะถูกโหลดใน div นี้ -->
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Thanat Portfolio</title>
+        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
 </html>
